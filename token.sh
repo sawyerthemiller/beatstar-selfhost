339f3a968097278cf31e4fa972a111ed057a383b
sawyerthemiller <dukes.of.ozaukee@gmail.com>
2025-11-05 16:19:37 -0600

    update token getter
    update token getter

---
+++ token.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pastebin raw URL
+TOKEN_URL="https://pastebin.com/raw/gKcng1Ky"
+
+# Fetch token
+TOKEN=$(curl -s $TOKEN_URL)
+
+# Write to project root
+echo "@externaladdress4401:registry=https://npm.pkg.github.com" > .npmrc
+echo "//npm.pkg.github.com/:_authToken=$TOKEN" >> .npmrc
+
+# Write to /website/beatstar
+echo "@externaladdress4401:registry=https://npm.pkg.github.com" > ./website/beatstar/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$TOKEN" >> ./website/beatstar/.npmrc
+
+echo "Token successfully downloaded"
