3b565c5dd0145750bfd5539d8daf7116f944b17e
sawyerthemiller <111768633+sawyerthemiller@users.noreply.github.com>
2025-11-07 11:29:10 -0600

    Update user.ts

--- website/beatstar/prisma/seeds/user.ts
+++ website/beatstar/prisma/seeds/user.ts
@@ -2,15 +2,18 @@ import prisma from '../../src/lib/prisma.js';
 import argon2 from '@node-rs/argon2';
 
 export const seed = async () => {
-	const user = {
-		id: 0,
-		uuid: '795b281a-ffa9-4d91-9042-eb7bae4c852e',
-		username: 'test',
-		password: await argon2.hash('test'),
-		admin: true,
-		starCount: 0
-	};
-	await prisma.user.create({
-		data: user
-	});
+  const user = {
+    id: 0,
+    uuid: '795b281a-ffa9-4d91-9042-eb7bae4c852e',
+    username: 'test',
+    password: await argon2.hash('test'),
+    admin: true,
+    starCount: 0,
+  };
+
+  await prisma.user.upsert({
+    where: { id: user.id },
+    update: {},
+    create: user,
+  });
 };
