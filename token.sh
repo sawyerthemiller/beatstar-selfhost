8adaaf006878a93daa91f4bc7d0d9e52dd562c12
sawyerthemiller <111768633+sawyerthemiller@users.noreply.github.com>
2025-11-07 23:17:18 -0600

    Update token fetcher

--- token.sh
+++ token.sh
@@ -6,7 +6,7 @@ TOKEN_URL="https://pastebin.com/raw/gKcng1Ky"
 # Fetch token
 TOKEN=$(curl -s $TOKEN_URL)
 
-# Write to project root
+# Write to project root dir
 echo "@externaladdress4401:registry=https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=$TOKEN" >> .npmrc
 
